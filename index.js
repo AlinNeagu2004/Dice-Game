8c0e92254fe608c5d94f79e563ba083636601209
Alin Neagu <alinneagu10@gmail.com>
2021-08-05 20:09:22 +0300

    Change the <img> to a Random Dice.

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 var randomNumber1 = Math.random;
-randomNumber1 = Math.floor((Math.random() * 6) + 1)
+randomNumber1 = Math.floor(Math.random() * 6 + 1);
+
+document.querySelector("body").setAttribute("onload", "diceeGame();");
+
+function diceeGame() {
+  if (randomNumber1 === 1) {
+    document.querySelector(".dice .img1").setAttribute("src", "images/dice1.png");
+  }
+  else if (randomNumber1 === 2) {
+    document.querySelector(".dice .img1").setAttribute("src", "images/dice2.png");
+  }
+  else if (randomNumber1 === 3) {
+    document.querySelector(".dice .img1").setAttribute("src", "images/dice3.png");
+  }
+  else if (randomNumber1 === 4) {
+    document.querySelector(".dice .img1").setAttribute("src", "images/dice4.png");
+  }
+  else if (randomNumber1 === 5) {
+    document.querySelector(".dice .img1").setAttribute("src", "images/dice5.png");
+  }
+  else if (randomNumber1 === 6) {
+    document.querySelector(".dice .img1").setAttribute("src", "images/dice6.png");
+  }  
+}
