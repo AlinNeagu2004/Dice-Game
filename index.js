b40d3b131918b36e2f987219d092ae94f81b3f64
Alin Neagu <alinneagu10@gmail.com>
2021-08-05 21:23:36 +0300

    Minor change in JS.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomDiceSource1 = "images/dice" + randomNumber1 + ".png";
-document.querySelectorAll("img")[0].setAttribute("src", randomDiceSource1);
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src", randomDiceSource1);
 
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var randomDiceSource2 = "images/dice" + randomNumber2 + ".png";
-document.querySelectorAll("img")[1].setAttribute("src", randomDiceSource2);
+var image2 = document.querySelectorAll("img")[0];
+image2.setAttribute("src", randomDiceSource2);
 
 if (randomNumber1 === randomNumber2) {
   document.querySelector("h1").innerHTML = "Draw!";
