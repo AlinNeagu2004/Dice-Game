45a85737457dab8a1b08dc5e107ebb863ad95e2c
Alin Neagu <alinneagu10@gmail.com>
2021-08-06 21:06:26 +0300

    Solved the refresh problem and the footer.

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
-var randomDiceSource1 = "images/dice" + randomNumber1 + ".png";
-var image1 = document.querySelectorAll("img")[0];
-image1.setAttribute("src", randomDiceSource1);
-
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var randomDiceSource2 = "images/dice" + randomNumber2 + ".png";
-var image2 = document.querySelectorAll("img")[1];
-image2.setAttribute("src", randomDiceSource2);
-
-if (randomNumber1 === randomNumber2) {
-  document.querySelector("h1").innerHTML = "Draw!";
+if (performance.getEntriesByType("navigation")[0].type === "reload") {
+  rollDice();
 }
-else if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
-}
-else {
-  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+
+function rollDice() {
+  var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+  var randomDiceSource1 = "images/dice" + randomNumber1 + ".png";
+  var image1 = document.querySelectorAll("img")[0];
+  image1.setAttribute("src", randomDiceSource1);
+
+  var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+  var randomDiceSource2 = "images/dice" + randomNumber2 + ".png";
+  var image2 = document.querySelectorAll("img")[1];
+  image2.setAttribute("src", randomDiceSource2);
+
+  if (randomNumber1 === randomNumber2) {
+    document.querySelector("h1").innerHTML = "Draw!";
+  } else if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins!";
+  } else {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+  }
 }
