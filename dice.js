599a7b8e6d686ff22fae7dd5751f9e363962ca00
Alin Neagu <alinneagu10@gmail.com>
2021-08-09 15:26:14 +0300

    Solved the problem with heading and btn.

--- dice.js
+++ dice.js
@@ -1,8 +1,15 @@
-// if (performance.getEntriesByType("navigation")[0].type === "reload") {
-//   rollDice();
-// }
+var element = document.querySelector("#btn");
 
-var button = document.getElementById("roll-button");
+// all CSS styles
+var styles_applied = window.getComputedStyle(element);
+
+if (styles_applied.display === "none") {
+  if (performance.getEntriesByType("navigation")[0].type === "reload") {
+    rollDice();
+  }
+} else if (styles_applied.display === "inline-block") {
+  rollDice();
+}
 
 function rollDice() {
   var randomNumber1 = Math.floor(Math.random() * 6) + 1;
@@ -22,6 +29,58 @@ function rollDice() {
   } else {
     document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
   }
-}
 
-button.addEventListener("click", rollDice);
+  if (randomNumber1 === randomNumber2) {
+    var draw = ["Refresh Me", "Refresh Me", "Draw!", "Draw!"];
+    var counter = 0;
+    var elem = document.querySelector("h1");
+    var inst = setInterval(changeDraw, 1000);
+
+    function changeDraw() {
+      elem.innerHTML = draw[counter];
+      counter++;
+      if (counter >= draw.length) {
+        counter = 0;
+        // clearInterval(inst);
+      }
+    }
+  } else if (randomNumber1 > randomNumber2) {
+    var firstWinner = [
+      "Refresh Me",
+      "Refresh Me",
+      "🚩 Player 1 Wins!",
+      "🚩 Player 1 Wins!",
+    ];
+    var counter = 0;
+    var elem = document.querySelector("h1");
+    var inst = setInterval(changeFirstWinner, 1000);
+
+    function changeFirstWinner() {
+      elem.innerHTML = firstWinner[counter];
+      counter++;
+      if (counter >= firstWinner.length) {
+        counter = 0;
+        // clearInterval(inst);
+      }
+    }
+  } else {
+    var secondWinner = [
+      "Refresh Me",
+      "Refresh Me",
+      "Player 2 Wins! 🚩",
+      "Player 2 Wins! 🚩",
+    ];
+    var counter = 0;
+    var elem = document.querySelector("h1");
+    var inst = setInterval(changeSecondWinner, 1000);
+
+    function changeSecondWinner() {
+      elem.innerHTML = secondWinner[counter];
+      counter++;
+      if (counter >= secondWinner.length) {
+        counter = 0;
+        // clearInterval(inst);
+      }
+    }
+  }
+}
